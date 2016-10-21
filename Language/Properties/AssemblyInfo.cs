e9e4cd066bde284888729c190ca18d7188f300a9
Denis Pavlichenko <denis@fuel9.com>
2016-10-21 09:25:17 -0500

    power BI, 1.3.0 RC1

--- Language/Properties/AssemblyInfo.cs
+++ Language/Properties/AssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.2.0.155")]
-[assembly: AssemblyVersion("1.2.0.155")]
-[assembly: AssemblyFileVersion("1.2.0.155")]
+// [assembly: AssemblyVersion("1.3.0.156")]
+[assembly: AssemblyVersion("1.3.0.156")]
+[assembly: AssemblyFileVersion("1.3.0.156")]
--- Management/Properties/AssemblyInfo.cs
+++ Management/Properties/AssemblyInfo.cs
@@ -41,5 +41,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.0.155")]
-[assembly: AssemblyFileVersion("1.2.0.155")]
+[assembly: AssemblyVersion("1.3.0.156")]
+[assembly: AssemblyFileVersion("1.3.0.156")]
