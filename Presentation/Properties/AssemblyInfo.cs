3b4fab19daf7427ed4444c154afe9292dc54f934
Denis Pavlichenko <denis@fuel9.com>
2017-02-11 08:44:00 -0600

    corrected build number

--- Presentation/Properties/AssemblyInfo.cs
+++ Presentation/Properties/AssemblyInfo.cs
@@ -41,5 +41,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.3.3.134")]
-[assembly: AssemblyFileVersion("1.3.3.134")]
+[assembly: AssemblyVersion("1.3.4.136")]
+[assembly: AssemblyFileVersion("1.3.4.136")]
