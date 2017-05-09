bf844007ebf2d2733e339cb1c9e139da0df277c3
Denis Pavlichenko <denis@fuel9.com>
2017-05-09 08:13:02 -0500

    updated build number

--- Language/Properties/AssemblyInfo.cs
+++ Language/Properties/AssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.4.0.143")]
-[assembly: AssemblyVersion("1.4.0.143")]
-[assembly: AssemblyFileVersion("1.4.0.143")]
+// [assembly: AssemblyVersion("1.4.0.145")]
+[assembly: AssemblyVersion("1.4.0.145")]
+[assembly: AssemblyFileVersion("1.4.0.145")]
